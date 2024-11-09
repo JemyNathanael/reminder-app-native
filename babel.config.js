c8cbc1acd1c61d41fa64350067459be1fda7ee4e
JemyNathanael <jemynathanael0821@gmail.com>
2024-11-10 00:35:34 +0700

    changed the home screen into list of reminders

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+    'react-native-reanimated/plugin',  // Ensure this is the last entry in the plugins array
+  ],
   };
 };
